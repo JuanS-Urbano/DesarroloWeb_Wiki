d0bb039d4ed0134d85b4049738864f032b30c895
Usuario <usuario@ejemplo.com>
2026-02-03 18:58:05 -0500

    Initial commit

---
+++ src/test/java/co/grupo1/proyecto/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grupo1.proyecto.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
